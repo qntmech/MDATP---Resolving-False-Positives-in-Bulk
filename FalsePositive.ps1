400e4b01ae897403f448b2f1b0a685db34a111e1
qntmech <qntmech@gmail.com>
2021-03-10 00:55:59 -0500

    Update FalsePositive.ps1

--- FalsePositive.ps1
+++ FalsePositive.ps1
@@ -1,6 +1,6 @@
-$tenantId = '<tenant ID>' ### Paste your own tenant ID here
-$appId = '<app ID>' ### Paste your own app ID here
-$appSecret = '<app Secret>' ### Paste your own app keys here
+$tenantId = '' ### Paste your own tenant ID here
+$appId = '' ### Paste your own app ID here
+$appSecret = '' ### Paste your own app keys here
 $resourceAppIdUri = 'https://api.securitycenter.windows.com'
 $oAuthUri = "https://login.windows.net/$TenantId/oauth2/token"
 
@@ -19,22 +19,21 @@ $contentType = "application/json"
 $token=$gett.access_token
 $header = @{"Authorization"="Bearer $token"}
 
-$splitFiles = Get-ChildItem "C:\temp\Pemex\SplitFiles"
+$splitFiles = Get-ChildItem "C:\Users\wkam\PycharmProjects\Pemex\split"
 	for($counter=0; $counter -lt $splitFiles.Count;$counter++)
 		{
 			$alertList = Get-Content $splitFiles[$counter].FullName #######  Paste in the location of a list of machines
 			$json = @{'comment' = 'Offboard'
 				  'status' = "Resolved"
-				  'assignedTo' = "email@domain.com"
-                                  'classification' = "FalsePositive"
-                                  'determination' = "Malware"
-                                  'comment' = "No Comment"
+				  'assignedTo' = "jose.noel@pemex.com"
+                  'classification' = "FalsePositive"
+                  'determination' = "Malware"        
 		} | ConvertTo-Json
 
 				foreach($alert in $alertList)
 					{
     						$alert    
-    						$answer = Invoke-RestMethod -Headers $header -Uri  https://api-us.securitycenter.windows.com/api/alerts/$alert -Method POST -Body $json -ContentType $contentType
+    						$answer = Invoke-RestMethod -Headers $header -Uri  https://api-us.securitycenter.windows.com/api/alerts/$alert -Method PATCH -Body $json -ContentType $contentType
 					}
 		Start-Sleep -Second 60
 		}
